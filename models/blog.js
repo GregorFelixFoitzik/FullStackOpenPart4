fdd12aa9fb961e98864e8055b2e1f9a91a2e24af
Gregor Foitzik <greg.foitzik@gmail.com>
2024-11-15 16:25:59 +0200

    4.2

--- models/blog.js
+++ models/blog.js
@@ -1,15 +1,4 @@
-require('dotenv').config()
 const mongoose = require('mongoose')
-const url = process.env.MONGODB_URI
-
-mongoose.connect(url)
-  .then(() => {
-    console.log('connected to MongoDB')
-  })
-  .catch(error => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
-
 
 const blogSchema = new mongoose.Schema({
   title: String,
